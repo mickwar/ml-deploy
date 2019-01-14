334f74a528f6255a9fc05630b8b81f80fcce58d8
mickey <mickey.a.warner@gmail.com>
2019-01-14 14:56:43 -0700

    add test data

--- src/data/gen_data.R
+++ src/data/gen_data.R
@@ -1,14 +1,28 @@
-### Fake data
+### Fake training data
 set.seed(1)
 n = 1000
 k = 5
-x = matrix(rnorm(n*k), n, k)
-y = as.matrix(sin(x[,1]) + cos(pi * x[,2]) + x[,3]^2 - x[,4])
+train_x = matrix(rnorm(n*k), n, k)
+train_y = as.matrix(sin(train_x[,1]) + cos(pi * train_x[,2]) + train_x[,3]^2 - train_x[,4])
 
-colnames(x) = paste0("x", 1:k)
-colnames(y) = "y"
+colnames(train_x) = paste0("x", 1:k)
+colnames(train_y) = "y"
 
-### Save the "raw" data
-### In production, we would expect to only have raw x's (i.e. unprocessed features)
-write.table(round(x, 3), "./raw_x.txt", quote = FALSE, row.names = FALSE)
-write.table(round(y, 3), "./raw_y.txt", quote = FALSE, row.names = FALSE)
+
+### Fake testing data
+m = 20
+test_x = matrix(rnorm(m*k), m, k)
+test_y = as.matrix(sin(test_x[,1]) + cos(pi * test_x[,2]) + test_x[,3]^2 - test_x[,4])
+
+colnames(test_x) = paste0("x", 1:k)
+colnames(test_y) = "y"
+
+
+### Save the "raw" training data
+write.table(round(train_x, 3), "./raw_train_x.txt", quote = FALSE, row.names = FALSE)
+write.table(round(train_y, 3), "./raw_train_y.txt", quote = FALSE, row.names = FALSE)
+
+### In production, we would expect to only have raw x's (i.e. unprocessed features),
+### but we use test_y so we can validate our model
+write.table(round(test_x, 3), "./raw_test_x.txt", quote = FALSE, row.names = FALSE)
+write.table(round(test_y, 3), "./raw_test_y.txt", quote = FALSE, row.names = FALSE)
